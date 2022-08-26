17bb917a0d1cc512776ecfe0f8fa534b2a9d44fa
unknown <pawanrahane999@gmail.com>
2022-08-26 14:30:45 +0530

    well come in github

---
+++ src/test/java/Mock1/New.java
@@ -0,0 +1,18 @@
+package Mock1;
+
+public class New {
+	public static void main(String[] args) {
+		String s= " This is my java program ";
+		int count =0;
+		for(int i = 0;i<=s.length()-1;i++) {
+			if (s.charAt(i)==' '||s.charAt(i)=='a'){
+				count++;
+			
+		}
+	}
+System.out.println(count);
+}
+
+	}
+
+
