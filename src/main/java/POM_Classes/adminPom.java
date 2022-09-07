b88ed3aa275f3380fab7bd0488ebcb5ea1b23dee
unknown <vsnehal771@gmail.com>
2022-09-07 16:13:05 +0530

    tc700 first

---
+++ src/main/java/POM_Classes/adminPom.java
@@ -0,0 +1,41 @@
+package POM_Classes;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class adminPom {
+
+	public class Pomclass1 {
+		// declaration
+			
+			@FindBy(xpath="//input[@class='_2IX_2- VJZDxU']")private WebElement uid;
+			@FindBy(xpath="//input[@type='password']")private WebElement pass;
+		    @FindBy(xpath="//button[@class='_2KpZ6l _2HKlqd _3AWRsL']")private WebElement clickonlogin;
+			@FindBy(xpath="//span[contains(text(),'enter valid')]")private WebElement getuiderrormsg;
+		    @FindBy(xpath="//span[contains(text(),'enter Pass')]")private WebElement getpasstext;
+			@FindBy(xpath="//button[contains(text(),'OTP')]")private WebElement requestotp;
+
+		// initialization
+		    public Pomclass1(WebDriver driver){
+		    	PageFactory.initElements(driver, this); 	
+		    }
+		// Declaration
+		    public void username(String user) {
+		    	uid.sendKeys(user);
+		    }
+		    public void password(String password) {
+		    	pass.sendKeys(password);
+		    }
+		    public void loginbtn() {
+		    	clickonlogin.click();
+		    }
+		    public String getmsguid() {
+		    	String uidmsg = getuiderrormsg.getText();
+		    	return uidmsg;
+		    }
+		}
+
+	
+}
---
+++ src/test/java/Test_Classes/testClass.java
@@ -0,0 +1,11 @@
+package Test_Classes;
+
+public class testClass {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to the github");
+	}
+
+
+
+}
